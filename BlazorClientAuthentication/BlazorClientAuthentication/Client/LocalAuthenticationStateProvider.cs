7f8ef5bb08c1977cba65526e394ee04ce88428e8
Rockford Lhotka <rocky@lhotka.net>
2022-06-15 00:33:16 -0500

    Add BlazorClientAuthentication solution

---
+++ BlazorClientAuthentication/BlazorClientAuthentication/Client/LocalAuthenticationStateProvider.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Components.Authorization;
+using System.Security.Claims;
+
+/// <summary>
+/// Basic implementation of AuthenticationStateProvider
+/// </summary>
+public class LocalAuthenticationStateProvider : AuthenticationStateProvider
+{
+  /// <summary>
+  /// Creates an instance of the type.
+  /// </summary>
+  public LocalAuthenticationStateProvider()
+  {
+    SetPrincipal(new ClaimsPrincipal());
+  }
+
+  private AuthenticationState AuthenticationState { get; set; } = 
+    new AuthenticationState(new ClaimsPrincipal());
+
+  /// <summary>
+  /// Gets the authentication state.
+  /// </summary>
+  /// <returns></returns>
+  public override Task<AuthenticationState> GetAuthenticationStateAsync()
+  {
+    return Task.FromResult(AuthenticationState);
+  }
+
+  /// <summary>
+  /// Sets the principal representing the current user identity.
+  /// </summary>
+  /// <param name="principal">ClaimsPrincipal instance</param>
+  public void SetPrincipal(ClaimsPrincipal principal)
+  {
+    AuthenticationState = new AuthenticationState(principal);
+    NotifyAuthenticationStateChanged(Task.FromResult(AuthenticationState));
+  }
+}
---
+++ BlazorClientAuthentication/BlazorClientAuthentication/Client/Program.cs
@@ -0,0 +1,16 @@
+using BlazorClientAuthentication.Client;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Services.AddScoped<AuthenticationStateProvider, LocalAuthenticationStateProvider>();
+builder.Services.AddAuthorizationCore();
+builder.Services.AddOptions();
+
+await builder.Build().RunAsync();
